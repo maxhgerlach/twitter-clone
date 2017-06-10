77b12ac49f846157bfd99fcb6bada9ae33cf7d2f
Max H. Gerlach <m@maxgerlach.de>
2017-06-10 12:13:53 +0200

    hello world web server

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+'use strict';
+
+var express = require('express');
+var app = express();
+
+// get route
+app.get('/', function(req, res) {
+    res.send('Hello world!');
+});
+
+// listen for requests
+app.listen(8080, function() {
+    console.log('Web server listening on port 8080!');
+});
