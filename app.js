1d9850d0081ade8647af6f66f8b8e1bff6cf199f
Max H. Gerlach <m@maxgerlach.de>
2017-06-10 15:32:39 +0200

    mysql and body-parser

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 'use strict';
 
+var mysql = require('mysql');
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'vagrant',
+    password: '',
+    database: 'twitter'
+});
+
 var app = express();
+
+connection.connect(function(err) {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    
+    console.log('Connected to the database.');
+    
+    app.listen(8080, function() {
+        console.log('Web server now listening on port 8080!');
+    });
+});
+
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // get route
 app.get('/', function(req, res) {
@@ -14,10 +39,14 @@ app.get('/', function(req, res) {
 
 // post route
 app.post('/tweets/create', function(req, res) {
+    var query = 'INSERT INTO Tweets(handle, body) VALUES(?, ?)';
+
+    var handle = req.body.handle;
+    var body = req.body.body;
+
+    console.log(handle);
+    console.log(body);
+    
     res.send('Creating tweet.');
 });
 
-// listen for requests
-app.listen(8080, function() {
-    console.log('Web server now listening on port 8080!');
-});
